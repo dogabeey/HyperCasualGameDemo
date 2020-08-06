2a9c01fbf3c47b5ee3fc4281f5693385f226f5fe
dogabeey@gmail.com <dogabeey@gmail.com>
2020-08-06 20:10:40 +0300

    - zoom mechanic. - smoother painting.

--- Assets/Scripts/DustCleaner.cs
+++ Assets/Scripts/DustCleaner.cs
@@ -6,8 +6,13 @@ public class DustCleaner : MonoBehaviour
 {
     public GameObject dustyObject;
     [Range(1,50)]public int brushSize = 40;
+    public string textureName;
+    public float zoomSensitivity = 30;
+
     Material material;
     Texture3D dustTexture;
+    Vector2 prevTouchDiff = Vector2.zero;
+    Vector2 currentTouchDiff;
 
     // Start is called before the first frame update
     void Start()
@@ -20,33 +25,59 @@ public class DustCleaner : MonoBehaviour
     {
         Texture2D tex;
         RaycastHit hit;
+
         if (Input.touches.Length == 1)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             if (Physics.Raycast(ray, out hit, 1000.0f, 1024))
             {
-                tex = (Texture2D) material.GetTexture("_MainTex");
-                Color[] colors = new Color[brushSize* brushSize];
-                for (int i = 0; i < colors.Length; i++)
-                {
-                    colors[0] = new Color(0.0f, 0.0f, 0.0f, 0.0f);
-                }
+                tex = (Texture2D) material.GetTexture(textureName);
 
                 Vector2 pixels = hit.textureCoord;
                 pixels.x *= tex.width;
                 pixels.y *= tex.height;
-                Debug.Log(pixels);
-                tex.SetPixels((int)pixels.x, (int)pixels.y, brushSize, brushSize, colors);
+                SetPixelGroup(tex, (int)pixels.x, (int)pixels.y, new Color(-1.0f, -1.0f, -1.0f, 1.0f), brushSize);
                 tex.Apply();
-                material.SetTexture("_MainTex", tex);
+                material.SetTexture(textureName, tex);
                 
             }
         }
         if (Input.touches.Length == 2)
         {
-            Vector2 avgInput = (Input.GetTouch(0).deltaPosition + Input.GetTouch(1).deltaPosition) / 2;
-            avgInput = new Vector2(avgInput.y, -avgInput.x);
-            dustyObject.transform.Rotate(avgInput, Space.World);
+            Vector2 delta0, delta1;
+            Vector2 avgInput = ((delta0 = Input.GetTouch(0).deltaPosition) + ( delta1 = Input.GetTouch(1).deltaPosition)) / 2;
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            Debug.Log(deltaMagnitudeDiff);
+
+            if (deltaMagnitudeDiff > 2 || deltaMagnitudeDiff < -2) Camera.main.fieldOfView += (deltaMagnitudeDiff / zoomSensitivity);
+            else
+            {
+                avgInput = new Vector2(avgInput.y, -avgInput.x);
+                dustyObject.transform.Rotate(avgInput, Space.World);
+            }
+        }
+    }
+
+    void SetPixelGroup(Texture2D texture, int x, int y, Color color, int brushSize)
+    {
+        for(int i = x - brushSize; i < x + brushSize;i++)
+        {
+            for (int j = y - brushSize; j < y + brushSize; j++)
+            {
+                texture.SetPixel(i, j, texture.GetPixel(i,j) - color);
+            }
         }
     }
 }
--- Assets/Shaders/MyShader.shader
+++ Assets/Shaders/MyShader.shader
@@ -1,46 +1,38 @@
-﻿Shader "Custom/MyShader" {
-	Properties{
-		_Color("Color", Color) = (1,1,1,1)
-		_Blend("Texture Blend", Range(0,1)) = 0.0
-		_MainTex("Albedo (RGB)", 2D) = "white" {}
-		_MainTex2("Albedo 2 (RGB)", 2D) = "white" {}
-		_Glossiness("Smoothness", Range(0,1)) = 0.5
-		_Metallic("Metallic", Range(0,1)) = 0.0
+﻿Shader "Custom/MyShader"
+{
+	Properties
+	{
+	   _Color("Main Color", Color) = (1,1,1,1)
+	   _MainTex("Base (RGB) Trans (A)", 2D) = "white" {}
+	   _BlendTex("Blend (RGB)", 2D) = "white"
+	   _BlendAlpha("Blend Alpha", float) = 0
 	}
-		SubShader{
-			Tags { "RenderType" = "Opaque" }
-			LOD 200
+		SubShader
+	   {
+		  Tags { "Queue" = "Geometry-9" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		  Lighting Off
+		  LOD 200
+		  Blend SrcAlpha OneMinusSrcAlpha
 
-			CGPROGRAM
-			// Physically based Standard lighting model, and enable shadows on all light types
-			#pragma surface surf Standard fullforwardshadows
+		  CGPROGRAM
+		  #pragma surface surf Lambert
 
-			// Use shader model 3.0 target, to get nicer looking lighting
-			#pragma target 3.0
+		  fixed4 _Color;
+		  sampler2D _MainTex;
+		  sampler2D _BlendTex;
+		  float _BlendAlpha;
 
-			sampler2D _MainTex;
-			sampler2D _MainTex2;
+		  struct Input {
+			float2 uv_MainTex;
+		  };
 
-			struct Input {
-				float2 uv_MainTex;
-				float2 uv_MainTex2;
-			};
+		  void surf(Input IN, inout SurfaceOutput o) {
+			fixed4 c = ((1 - _BlendAlpha) * tex2D(_MainTex, IN.uv_MainTex) + _BlendAlpha * tex2D(_BlendTex, IN.uv_MainTex)) * _Color;
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		  }
+		  ENDCG
+	   }
 
-			half _Blend;
-			half _Glossiness;
-			half _Metallic;
-			fixed4 _Color;
-
-			void surf(Input IN, inout SurfaceOutputStandard o) {
-				// Albedo comes from a texture tinted by color
-				fixed4 c = lerp(tex2D(_MainTex, IN.uv_MainTex), tex2D(_MainTex2, IN.uv_MainTex2), _Blend) * _Color;
-				o.Albedo = c.rgb;
-				// Metallic and smoothness come from slider variables
-				o.Metallic = _Metallic;
-				o.Smoothness = _Glossiness;
-				o.Alpha = c.a;
-			}
-			ENDCG
-		}
-			FallBack "Diffuse"
+		   Fallback "Transparent/VertexLit"
 }
